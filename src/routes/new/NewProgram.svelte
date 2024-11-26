89d2d05afd4d0f60cdb855b6bfeb13ab2afdad70
jonas <jonas.voisard@gmail.com>
2024-11-26 17:44:11 +0100

    I can run wasm

--- src/routes/new/NewProgram.svelte
+++ src/routes/new/NewProgram.svelte
@@ -1,34 +1,78 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
+    import { init, WASI } from '@wasmer/wasi'
+    import { Buffer } from 'buffer'
+    import { resolve } from 'chart.js/helpers'
+    import type { Move } from '$lib/move'
+
+    onMount(() => {
+        window.Buffer = window.Buffer || Buffer
+    })
+
+    let file: File | null = null
+
     async function selectProgram() {
-        console.log(Module)
         const [fileHandle] = await window.showOpenFilePicker({
             types: [
                 {
-                    description: 'Web Assembly Push Swap',
+                    description: 'Web Assembly',
                     accept: { 'application/wasm': '.wasm' },
                 },
             ],
             multiple: false,
         })
-        const file = await fileHandle.getFile()
+        file = await fileHandle.getFile()
+        console.log(file)
+    }
+
+    async function runProgram(values: number[]): Promise<Move[]> {
+        if (!file) return []
         const buffer = await file.arrayBuffer()
-        const importObject = {
-            wasi_snapshot_preview1: {
-                imported_func: (arg) => console.log('imported_func', arg),
-                args_sizes_get: (arg) => console.log('args_sizes_get', arg),
-                args_get: (arg) => console.log('args_get', arg),
-                proc_exit: (arg) => console.log('proc_exit', arg),
-                fd_write: (arg) => console.log('fd_write', arg),
-            },
-        }
-        const obj = await WebAssembly.instantiate(buffer, importObject)
-        const res = obj.instance.exports._start()
-        console.log(res)
+        const module = await WebAssembly.compile(buffer)
+        await init()
+
+        let wasi = new WASI({
+            env: {},
+            args: ['hey', ...values.map((v) => v.toString())],
+        })
+        wasi.instantiate(module, {})
+        const exitCode = wasi.start()
+        if (exitCode == 1) throw Error('Program exit with exit code [1]')
+        let stdout = wasi.getStdoutString()
+        console.log(stdout)
+
+        // TODO: valid the output with zod
+        return stdout.split('\n') as Move[]
     }
 </script>
 
 <fieldset class="p-4 border rounded border-primary">
     <legend>New Program</legend>
 
-    <button class="btn" onclick={selectProgram}> Import </button>
+    <div class="flex gap-2">
+        {#if file}
+            <label class="block w-full">
+                <input
+                    readonly
+                    disabled
+                    class="input input-bordered w-full"
+                    value={file.name}
+                    min="2"
+                />
+            </label>
+        {/if}
+
+        <button
+            class="btn"
+            class:w-full={!file}
+            class:btn-primary={!file}
+            onclick={selectProgram}
+        >
+            Select
+        </button>
+    </div>
+
+    <button class="btn" onclick={() => runProgram([2, 3, 5, 9, 6])}>
+        test
+    </button>
 </fieldset>
