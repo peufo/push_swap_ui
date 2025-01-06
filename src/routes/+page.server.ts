c028f65f533aa34c8febdb78222d125f8049d935
jonas <jonas.voisard@gmail.com>
2025-01-06 18:06:31 +0100

    sort algos from best to bad

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,12 @@
-import { redirect } from '@sveltejs/kit'
 import { prisma } from '$lib/server'
 
 export const load = async () => {
     return {
         algos: await prisma.algorithm.findMany({
             include: { author: true },
+            orderBy: {
+                score500: 'asc',
+            },
         }),
     }
 }
